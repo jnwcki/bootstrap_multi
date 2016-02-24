fba756134fd266dd173490f757f0cb3f8409356b
jnwcki <jude.nowicki@gmail.com>
2016-02-24 14:02:16 -0500

    Blank project ready

--- bootstrap_app/views.py
+++ bootstrap_app/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, render_to_response
+
 
 # Create your views here.
+
+def index_view(request):
+    return render_to_response("index.html", {})
+    # return HttpResponse("OMGITSAWEBSITE!!!!!!")
