faa63362c82807006b4d0ee9750405d300d25424
jnwcki <jude.nowicki@gmail.com>
2016-02-28 16:34:25 -0500

    completed assignment basic requirements

--- bootstrap_app/views.py
+++ bootstrap_app/views.py
@@ -16,13 +16,16 @@ def about_me_view(request):
 
 
 def deer_leap_view(request):
-    deer_data = Trail.objects.filter(name='Deer Leap')
+    deer_data = Trail.objects.get(name='Deer Leap')
+    print(deer_data)
     return render_to_response("deer_leap_view.html", {"deer_data": deer_data})
 
 
 def pico_view(request):
-    return render_to_response("pico_view.html", {})
+    pico_data = Trail.objects.get(name='Pico Peak')
+    return render_to_response("pico_view.html", {'pico': pico_data})
 
 
 def mbr_view(request):
-    return render_to_response("mbr.html", {})
+    mbr_data = Trail.objects.get(name="Mount Tom")
+    return render_to_response("mbr.html", {'mbr': mbr_data})
